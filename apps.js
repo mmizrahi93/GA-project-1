51399cf08733d7365af1c49e02d146deae4ad3ae
Matan Mizrahi <matanmizrahi@Matans-MacBook-Air.local>
2021-05-10 18:59:20 -0400

    slowly getting there

--- apps.js
+++ apps.js
@@ -3,31 +3,48 @@ const table = document.querySelector('table')
 container.append(table)
 
 //array method for switching players I got from the tic tac toe hw
-const redPlayer = [];
-const blackPlayer = [];
+const playerOne = [];
+const playerTwo = [];
 
 // click method was also code i wrote in the tic tac toe hw
 const click = (ev) => {
-    if (ev.currentTarget.classList.contains("start")) {
-        if ((ev.currentTarget.style.backgroundColor === "red") || (ev.currentTarget.style.backgroundColor === "black")) {
+        if ((ev.currentTarget.classList.contains("red")) || (ev.currentTarget.classList.contains("black"))) {
             return;
-        } else if(redPlayer.length === blackPlayer.length) {
+        } else if(playerOne.length === playerTwo.length) {
             // ev.currentTarget.querySelector(`#${7}`).classList.add(".row1")
             // ev.currentTarget.style.backgroundColor = "red"
-            redPlayer.push(ev)
+            playerOne.push(ev)
             ev.currentTarget.classList.add("red")
             console.log(ev.currentTarget)
            
-        } else if(redPlayer.length > blackPlayer.length) {
+        } else if(playerOne.length > playerTwo.length) {
             // ev.currentTarget.querySelector(`#${+ 7}`).classList.add(".row1")
             // ev.currentTarget.style.backgroundColor = "black"
-            blackPlayer.push(ev)
+            playerTwo.push(ev)
             ev.currentTarget.classList.add("black")
             console.log(ev.currentTarget)
         }
 
-    } else {
+     else {
         alert("Cant go there!")
+    }
+    
+}
+
+
+
+const gameBoard = [
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""]
+]
+
+const gameFunction = (ev) => {
+    if(gameBoard[i][n].length !== 0) {
+
     }
 }
 
